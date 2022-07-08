25b764163d8f6efd24355e1f42023fe62344cd3a
sl-govau <105312850+sl-govau@users.noreply.github.com>
2022-07-08 13:47:39 +0000

    update package exclusion

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     python_requires=">=3.8",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
-    packages=find_packages(".", exclude=["test", "tests", "extractors"]),
+    packages=find_packages(".", exclude=["tests", "demo_extractors"]),
     include_package_data=True,
     install_requires=[
         r.strip() for r in open("requirements.txt", "r") if not r.startswith("#")
