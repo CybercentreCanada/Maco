da3de3689a44c64b6e248a73bc74b8fe8c8077e5
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-07-15 06:58:57 +0000

    Create a subdirectory for model_only setup

--- model_setup/setup.py
+++ model_setup/setup.py
@@ -5,13 +5,11 @@ from setuptools import setup
 
 setup(
     python_requires=">=3.8",
-    use_scm_version=True,
+    use_scm_version={"root": ".."},
     setup_requires=["setuptools_scm"],
     packages=['maco.model'],
     include_package_data=True,
-    install_requires=[
-        r.strip() for r in open("requirements.txt", "r") if not r.startswith("pydantic")
-    ],
+    install_requires=["pydantic==1.10.9"],
     name="maco-model",
     description="",
     author="",
