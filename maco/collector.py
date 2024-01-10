7fb1b2f1eac1bf030850a165e24d04f55918dbee
cccs-rs <ryan.samaroo@cyber.gc.ca>
2024-01-10 13:32:13 +0000

    Add support for running extractors within venvs

--- maco/collector.py
+++ maco/collector.py
@@ -1,10 +1,14 @@
 """Convenience functions for discovering your extractors."""
 import importlib
 import inspect
+import json
 import logging
 import os
 import pkgutil
+import subprocess
 import sys
+from glob import glob
+from tempfile import NamedTemporaryFile
 from typing import Any, BinaryIO, Dict, List
 from pydantic import BaseModel
 
@@ -19,6 +23,37 @@ class ExtractorLoadError(Exception):
 
 logger = logging.getLogger("maco.lib.helpers")
 
+
+VENV_SCRIPT = """
+import importlib
+import json
+import os
+import sys
+import yara
+
+from base64 import b64encode
+parent_package_path = os.path.dirname(__file__).rsplit("{module_name}".split('.', 1)[0], 1)[0]
+sys.path.insert(1, parent_package_path)
+mod = importlib.import_module("{module_name}")
+
+class Base64Encoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, bytes):
+            return b64encode(o).decode()
+        return json.JSONEncoder.default(self, o)
+
+result = mod.{module_class}().run(open("{sample_path}", 'rb'), matches=yara.compile(source=mod.{module_class}.yara_rule).match("{sample_path}"))
+
+with open("{output_path}", 'w') as fp:
+    if not result:
+        json.dump(dict(), fp)
+    else:
+        try:
+            json.dump(result.model_dump(exclude_defaults=True, exclude_none=True), fp, cls=Base64Encoder)
+        except AttributeError:
+            # venv likely has an older version of Pydantic < 2 installed
+            json.dump(result.dict(exclude_defaults=True, exclude_none=True), fp, cls=Base64Encoder)
+"""
 
 def _verify_response(resp: BaseModel) -> Dict:
     """Enforce types and verify properties, and remove defaults."""
@@ -51,13 +86,13 @@ class Collector:
         self.extractors = self._find_extractors()
 
         # compile yara rules gathered from extractors
-        rules_merged = "\n".join([x.yara_rule for x in self.extractors.values()])
+        rules_merged = "\n".join([x["module"].yara_rule or '' for x in self.extractors.values()])
         self.rules = yara.compile(source=rules_merged)
 
         # map rule names to extractors, since each extractor can have multiple rules
         self.rule_map = {}
         for k, v in self.extractors.items():
-            self.rule_map.update({r: k for r in v.yara_rule_names})
+            self.rule_map.update({r: k for r in v["module"]().yara_rule_names})
 
     def _find_extractors(self):
         """Find extractors from the supplied path."""
@@ -80,9 +115,30 @@ class Collector:
         logger.debug(f"{sys.path=}")
         mod = importlib.import_module(foldername)
 
+        root_venv = None
+        if "venv" in os.listdir(self.path):
+            root_venv = os.path.join(self.path, "venv")
+
+        def find_venv(path: str) -> str:
+            parent_dir = os.path.dirname(path)
+            if "venv" in os.listdir(path):
+                # venv is in the same directory as the parser
+                return os.path.join(path, "venv")
+            elif parent_dir == self.path or path == self.path:
+                # We made it all the way back to the parser directory
+                # Use root venv, if any
+                return root_venv
+            elif "venv" in os.listdir(parent_dir):
+                # We found a venv before going back to the root of the parser directory
+                # Assume that because it's the closest, it's the most relevant
+                return os.path.join(parent_dir, "venv")
+            else:
+                # Keep searching in the parent directory for a venv
+                return find_venv(parent_dir)
+
         # walk packages in the extractors directory to find all extactors
         extractors = {}
-        for _, module_name, ispkg in pkgutil.walk_packages(
+        for module_path, module_name, ispkg in pkgutil.walk_packages(
             mod.__path__, mod.__name__ + "."
         ):
             if ispkg:
@@ -94,11 +150,23 @@ class Collector:
             logger.debug(f"inspecting '{module_name}' for extractors")
             # raise an exception if one of the potential extractors can't be imported
             # note that excluding an extractor through include/exclude does not prevent it being imported
+
+            # Local site packages, if any, need to be loaded before attempting to import the module
+            parser_venv = find_venv(module_path.path)
+            parser_site_packages = None
+            if parser_venv:
+                for dir in glob(os.path.join(parser_venv, "lib/python*/site-packages")):
+                    sys.path.insert(1, dir)
+                    break
             try:
                 module = importlib.import_module(module_name)
             except Exception as e:
-                logger.warning(f"Problem importing module '{module_name}': {e}")
+                # Log if there was an error importing module
+                logger.error(f"{module_name}: {e}")
                 continue
+            finally:
+                if parser_site_packages in sys.path:
+                    sys.path.remove(parser_site_packages)
 
             # find extractors in the module
             for _, member in inspect.getmembers(module):
@@ -108,6 +176,10 @@ class Collector:
                 if not issubclass(member, extractor.Extractor):
                     # not an extractor
                     continue
+                if issubclass(member, extractor.Extractor) and not member.author:
+                    # not a valid extractor
+                    continue
+
                 # check if we want this extractor
                 name = member.__name__
                 if self.exclude and name in self.exclude:
@@ -118,7 +190,7 @@ class Collector:
                     continue
                 # initialise and register
                 logger.debug(f"register '{name}'")
-                extractors[name] = member()
+                extractors[name] = dict(module=member, venv=parser_venv, module_path=module_path.path)
         logger.debug(f"found extractors {list(extractors.keys())}\n")
         if not extractors:
             raise ExtractorLoadError("no extractors were loaded")
@@ -149,7 +221,50 @@ class Collector:
         extractor = self.extractors[extractor_name]
         resp = None
         try:
-            resp = extractor.run(stream, matches)
+            if extractor["venv"]:
+                # Snippet from configextractor-py
+
+                extractor_module = extractor["module"]
+                # Write temporary script in the same directory as extractor to resolve relative imports
+                python_exe = os.path.join(extractor["venv"], "bin", "python")
+                with NamedTemporaryFile() as sample_path:
+                    sample_path.write(stream.read())
+                    sample_path.flush()
+
+                    with NamedTemporaryFile("w", dir=extractor["module_path"], suffix=".py") as script:
+                        with NamedTemporaryFile() as output:
+                            module_name = extractor_module.__module__
+                            module_class = extractor_module.__name__
+                            script.write(
+                                VENV_SCRIPT.format(
+                                    module_name=module_name,
+                                    module_class=module_class,
+                                    sample_path=sample_path.name,
+                                    output_path=output.name,
+                                )
+                            )
+                            script.flush()
+                            custom_module = (
+                                script.name.split(".py")[0].replace(f"{self.path}/", "").replace("/", ".")
+                            )
+                            proc = subprocess.run(
+                                [python_exe, "-m", custom_module],
+                                cwd=self.path,
+                                capture_output=True,
+                            )
+                            try:
+                                # Load results and return them
+                                output.seek(0)
+                                return json.load(output)
+                            except Exception:
+                                # If there was an error raised during runtime, then propagate
+                                delim = f'File "{extractor["module_path"]}"'
+                                exception = proc.stderr.decode()
+                                if delim in exception:
+                                    exception = f"{delim}{exception.split(delim, 1)[1]}"
+                                raise Exception(exception)
+            else:
+                resp = extractor["module"]().run(stream, matches)
         except Exception:
             # caller can deal with the exception
             raise
