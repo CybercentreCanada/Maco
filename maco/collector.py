7074fd62ef7b3c0315084c7243d004ff0f9611d5
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2026-03-02 17:30:43 +0000

    Correct formatting

--- maco/collector.py
+++ maco/collector.py
@@ -1,4 +1,5 @@
 """Convenience functions for discovering your extractors."""
+
 from __future__ import annotations
 
 import inspect
--- maco/extractor.py
+++ maco/extractor.py
@@ -1,4 +1,5 @@
 """Base class for an extractor script."""
+
 from __future__ import annotations
 
 import logging
